bcda507a214256731eda1d0ae293b46ef4d1b32c
Pierre-Louis Serré <plouiserre@gmail.com>
2021-01-01 19:16:00 +0100

    read homepage of jeuxvideo.com

---
+++ httpManager.go
@@ -0,0 +1,41 @@
+package main 
+
+import(
+	"net/http"
+	"io/ioutil"
+	"fmt"
+)
+
+type httpManager struct {
+	url string
+	contentPage []byte
+}
+
+func (httpManager *httpManager) getContentPage(){
+	if len(httpManager.url) > 0 {
+		resp, err := http.Get(httpManager.url)
+
+		if err != nil {
+			fmt.Println("Something bad happen")
+		} else {
+			httpManager.readGetResult(resp)
+		}
+	} else {
+		fmt.Println("You must defined an url")
+	}
+}
+
+func (httpManager httpManager) readGetResult(resp *http.Response){
+
+	defer resp.Body.Close()
+	
+	html, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println("Something bad happen")
+	}else {
+		httpManager.contentPage = html
+		fmt.Println("Content of the page !!!")
+		fmt.Printf("%s\n", httpManager.contentPage)
+	}
+}
